cfaaf5dd2f11a7baf7aecbbeaf0088635c2dfba2
Nostalgick0 <gaennupi@gmail.com>
2026-05-19 20:41:15 -0700

    Index.jsx

---
+++ src/pages/Index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography, Card, Row, Col} from "antd";
+const { Title } = Typography;
+
+
+function Index(){
+    return(
+        <div>
+            <Title>Hola Mundo</Title>
+        </div>
+    );
+}
+
+export default Index;
